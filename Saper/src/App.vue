299905986706d523534d77ea7677533a3e6384cb
Canthar90 <d.cieslak90@gmail.com>
2023-10-10 13:46:29 +0200

    Some basic settings for project

--- Saper/src/App.vue
+++ Saper/src/App.vue
@@ -7,11 +7,8 @@
       <game-menu class="mt-24"></game-menu>
     </div>
   </body>
-
-  <RouterView />
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import GameMenu from './views/GameMenu.vue'
 </script>
--- Saper/src/views/GameMenu.vue
+++ Saper/src/views/GameMenu.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="flex justify-center p-4">
-    <div class="bg-slate-400 flex justify-center">
-      <div class="bg-slate-800 text-red-700">10:00</div>
-      <div>ANY</div>
-      <div>ANY</div>
+    <div class="grid gap-4 grid-rows-1 grid-cols-3 bg-slate-400 justify-center">
+      <div
+        class="border border-solid border-black bg-slate-800 text-red-700 p-2 border-spacing-2 lining-nums text-xl"
+      >
+        10:00
+      </div>
+      <div
+        class="border border-solid border-black border-spacing-2 p-2 flex justify-center"
+        role="button"
+        @click="GameReset"
+      >
+        {{ gameEmoji }}
+      </div>
+      <div
+        class="p-2 border-spacing-2 border border-solid border-black bg-slate-800 text-red-700 flex justify-center lining-nums text-xl"
+      >
+        {{ mineCoversLeft.toString().padStart(3, '0') }}
+      </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+let gameEmoji = '😊'
+let mineCoversLeft: Ref<number> = ref(10)
+const timeLeft = {
+  minutes: 10,
+  seconds: 0
+}
+
+let mineCoversLeftString = ref(mineCoversLeft.value.toString().padStart(3, '0'))
+
+const GameReset = () => {
+  console.log('Placeholder')
+  mineCoversLeft.value--
+}
+</script>
