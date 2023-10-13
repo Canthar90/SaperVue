099b7276e5a6709a270eb5751430a9e2c8fd3432
Canthar90 <d.cieslak90@gmail.com>
2023-10-13 16:47:43 +0200

    Added game Segments declarations

--- Saper/src/views/GameMenu.vue
+++ Saper/src/views/GameMenu.vue
@@ -24,30 +24,57 @@
       </div>
     </div>
   </div>
-  <div :class="gridColsNum">
-    <div v-for="i in numberOfXSegments" key="i">
-      <div v-for="j in numberOfYSegments" key="j">{{ i }} - {{ j }}</div>
+
+  <div class="flex justify-center">
+    <div :class="gridColsNum">
+      <div v-for="seg in gameSegments" :key="seg.id" :id="String(seg.id)" :class="seg.styles">
+        {{ seg.sign }}
+      </div>
     </div>
   </div>
   <!-- <div class="grid justify-center" :class="{grid-rows-{{ numberOfXSegments }}}"></div> -->
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import type { Ref } from 'vue'
+
+type Segment = { id: number; styles: string; isABomb: boolean; sign: string }
 
 const numberOfXSegments = ref(8)
 const numberOfYSegments = ref(8)
 const numberOfMines = ref(10)
+const gameSegments: Ref<Segment[]> = ref([])
 
-const gridColsNum = computed(
-  () =>
-    `grid justify-center gap-2 grid-rows-${numberOfXSegments.value} grid-cols-${numberOfYSegments.value} text-xl`
-)
+onMounted(() => GenerateSegments())
 
-// grid grid-rows-${String(numberOfXSegments)} grid-cols-${String(numberOfYSegments)}
+function GenerateSegments() {
+  let idNr = 1
+  for (let i = 1; i < numberOfXSegments.value + 1; i++) {
+    for (let j = 1; j < numberOfYSegments.value + 1; j++) {
+      const Segment: Segment = {
+        id: idNr,
+        styles: 'border border-solid border-black bg-slate-600 rounded-lg p-6 flex justify-center',
+        isABomb: false,
+        sign: ''
+      }
+      gameSegments.value.push(Segment)
 
-// const gridRowsNum = ref(`grid-cols-${numberOfYSegments}`)
+      idNr++
+    }
+  }
+}
+
+const gridColsNum = computed(() => {
+  let numberOfRows = 'grid-rows-' + String(numberOfXSegments.value)
+  let numberOfCols = 'grid-cols-' + String(numberOfYSegments.value)
+  console.log(numberOfCols)
+  console.log(typeof numberOfCols)
+
+  return `grid gap-2 justify-center grid-rows-8 grid-cols-8`
+})
+
+// ${String(numberOfXSegments.value)}
 
 const gameEmoji: Ref<string> = ref('😊')
 let mineCoversLeft: Ref<number> = ref(10)
